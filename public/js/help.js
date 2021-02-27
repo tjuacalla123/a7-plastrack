0d55fe5d9d58e841f65606c132526221b418bb11
Jane Dinh <janelamdinh@gmail.com>
2021-02-27 00:21:33 -0800

    added toggle click event to help and tips page

--- public/js/help.js
+++ public/js/help.js
@@ -1,11 +1,10 @@
 $( document ).ready(function() {
     $('.section').each(function() {
         var descDiv = $(this).children('.descriptions');
-       descDiv.hide(); // Sets initial desc to hide. You can alternatively do this via css such that all .desc { display: none; }.
-        var titleDiv = $(this).children('.help-titles'); // Remove if you do not want to hide original text upon toggling
+       descDiv.hide(); 
+        var titleDiv = $(this).children('.help-titles'); 
         $(this).click(function(e) {
              descDiv.toggle();
-            // titleDiv.toggle(); // Remove if don't want to hide title upon toggling
         });
     });
 });
