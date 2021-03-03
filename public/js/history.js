021c4619e65c27369abaa10824b36e07de6ddd00
tjuacalla123 <>
2021-03-02 23:02:09 -0800

    started on log history

--- public/js/history.js
+++ public/js/history.js
@@ -1,12 +1,18 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	// load circles 
-	var data = JSON.parse(localStorage.getItem("data"));
-	console.log(data);
-	var statsStuff = {};
+	// get data from localStorage
+	var dataset = JSON.parse(localStorage.getItem("data"));
+	today(dataset);
+	
+})	
+
+
+function today(dataset) {
 	// generate today's stats
-	var today = data[moment().format('YYYY MM DD')];
+	var statsStuff = {};
+
+	var today = dataset[moment().format('YYYY MM DD')];
 	for (log in today) {
 		var allLogs = today[log]; // very fast runtime
 		for (item in allLogs) {
@@ -21,24 +27,104 @@ $(document).ready(function() {
 			}
 		}
 	}
+	var modified = [];
+	for (i in statsStuff) {
+		minCount
+		var withName = statsStuff[i];
+		withName.push(i);
+		modified.push(withName);
+	}
+	var counts = modified.map(function(value) { 
+		return value[0]; 
+	});
+	var maxCount = Math.max.apply(Math,counts);
+	var minCount = Math.min.apply(Math,counts);
+	bubbleChart(modified, minCount, maxCount);
 	
-console.log(statsStuff);
-	
-	//$("<circle>", {id: "foo", "class": "a"});
-	
-	
-	
-})
+}
 
-/*
-(function() {
-	var height = 500;
-	var width = 500;
+function lastXDays(dataset) { // not done yet
+	var statsStuff = {};
+	var allData = dataset;
+	var keyArr = Object.keys(allData);
+	var sorted  = keyArr.sort((a,b) => new moment(a).format('YYYY MM DD') - new moment(b).format('YYYY MM DD'));
+	for (log in allToday) {
+		var allLogs = today[log]; // very fast runtime
+		for (item in allLogs) {
+			var plastic = allLogs[item];
+			var keyName = plastic["name"] + " ("+ plastic["size"] +")";
+			var val = statsStuff[keyName];
+			if (val == undefined) {
+				statsStuff[keyName] = [plastic["count"], plastic["size"]]
+			}
+			else {
+				statsStuff[keyName][0] += plastic["count"];
+			}
+		}
+	}
+	var modified = [];
+	for (i in statsStuff) {
+		minCount
+		var withName = statsStuff[i];
+		withName.push(i);
+		modified.push(withName);
+	}
+	var counts = modified.map(function(value) { 
+		return value[0]; 
+	});
+	var maxCount = Math.max.apply(Math,counts);
+	var minCount = Math.min.apply(Math,counts);
+}
+
+
+
+
+
+
+
+function bubbleChart(dataset, minDomain, maxDomain) {
+	
+	console.log($(".graph-box").css("height"));
+	console.log($(".graph-box").css("width"));
+	var width = $(".graph-box").css("width");
+	var height = $(".graph-box").css("height");
 	var svg = d3.select(".graph-box")
-		.append("svg")
-		.attr("height", height);
-		.attr("width", width);
-		.append("g")
-		.attr("transform", "translate(0,0)")
-})
-*/
+			.append("svg")
+			.attr("height", height)
+			.attr("width", width)
+			.append("g")
+			.attr("transform", "translate(0,0)");
+			
+	// decides positioning of circles	
+	var simulation = d3.forceSimulation()
+			.force("x", d3.forceX(345/2).strength(0.05))
+			.force("y", d3.forceY(345/2).strength(0.05))
+			.force("collide", d3.forceCollide(function(d) {
+				return radiusScale(d[0])
+			}))
+	
+	var radiusScale = d3.scaleSqrt().domain([minDomain, maxDomain]).range([20,50])
+	
+	var circles = svg.selectAll(".plastic")
+			.data(dataset)
+			.enter()
+			.append("circle")
+			.attr("class","plastic")
+			.attr("r", function(d) {
+				return radiusScale(d[0])
+			})
+			.attr("fill", "lightblue");
+			
+	simulation.nodes(dataset)
+			.on('tick', ticked)
+			
+	function ticked() {
+		circles
+			.attr("cx", function(d) {
+				return d.x;
+			})
+			.attr("cy", function(d) {
+				return d.y;
+			});
+	}
+}
